421f300bd62c3f5d659e4b9fc93d2fe6b66763cf
Brienyll <brienyll29@gmail.com>
2018-02-15 20:24:13 -0800

    added method

--- objectClasses.java
+++ objectClasses.java
@@ -1,6 +1,10 @@
 class Person {
     String name;
     int age;
+    
+    void speak() {
+    System.out.println("My name is " + name + " I am " + age + " years old.");
+    }
 }
 
 public class App {
@@ -8,6 +12,7 @@ public class App {
         Person person1 = new Person();
         person1.name = "Brix Angeles;
         person1.age = 35;
+        person1.speak();
     
         System.out.println(person1.name);
     }
