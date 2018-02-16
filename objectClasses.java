fdc300cf25fe4764e2b9729efb895620578de635
Brienyll <brienyll29@gmail.com>
2018-02-15 20:20:12 -0800

    Create objectClasses.java

---
+++ objectClasses.java
@@ -0,0 +1,12 @@
+class Person {
+    String name;
+    int age;
+}
+
+public class void main(String[] args) {
+    Person person1 = new Person();
+    person1.name = "Brix Angeles;
+    person1.age = 35;
+    
+    System.out.println(person1.name);
+}
